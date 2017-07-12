a8dcf4fff281cf118fb6e3b0fd563abb7ceeda90
willclarktech <willclarktech@users.noreply.github.com>
2017-07-12 11:25:21 +0200

    Add custom rules

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 module.exports = {
-  extends: ['airbnb-base'],
+	extends: ['airbnb-base'],
+	rules: {
+		camelcase: ['error', { properties: 'always' }],
+		eqeqeq: ['error', 'always'],
+		indent: ['error', 'tab'],
+		'new-cap': ['error', { newIsCap: true, capIsNewExceptionPattern: 'should[.(]' }],
+		'no-console': ['error', { allow: ['error', 'info', 'warn'] }],
+		'no-tabs': 'off',
+	},
 };
