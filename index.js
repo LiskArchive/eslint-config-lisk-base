e565e089c57157bcb250440d86ba28d00de66e0b
willclarktech <willclarktech@users.noreply.github.com>
2017-12-11 18:03:31 +0100

    :nail_care: Turn off rules that conflict with Prettier
    Closes #11

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 module.exports = {
 	extends: ['airbnb-base'],
 	rules: {
+		// Our preferences
 		'arrow-parens': ['error', 'as-needed'],
 		camelcase: ['error', { properties: 'always' }],
 		eqeqeq: ['error', 'always'],
-		indent: ['error', 'tab', { SwitchCase: 1 }],
 		'new-cap': ['error', { newIsCap: true, capIsNewExceptionPattern: 'should[.(]' }],
 		'no-console': ['error', { allow: ['error', 'info', 'warn'] }],
 		'no-tabs': 'off',
 
+		// Plugin rules
 		'import/order': 'error',
-
 		'mocha/handle-done-callback': 'error',
 		'mocha/max-top-level-suites': 'warn',
 		'mocha/no-exclusive-tests': 'error',
@@ -23,5 +23,13 @@ module.exports = {
 		'mocha/no-skipped-tests': 'error',
 		'mocha/no-synchronous-tests': 'error',
 		'mocha/no-top-level-hooks': 'error',
+
+		// Prettier conflicts
+		curly: 'off',
+		'function-paren-newline': 'off',
+		indent: 'off',
+		'no-confusing-arrow': 'off',
+		'no-mixed-operators': 'off',
+		'object-curly-newline': 'off',
 	},
 };
